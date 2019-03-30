b9bcc1ae0e76060a95c6af7ae58583cf6bcf0916
vagrant <vagrant@homestead>
2019-03-30 22:46:28 +0000

    mapped some api rroutes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Listing;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('listing/{listing}', function(Listing $listing) {
+    return $listing->toJson();
 });
