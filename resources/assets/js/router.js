4ce1ae8dad05adfa604fc095c008f2368ad7140f
John (Jef) Fisher <Fishe545@gmail.com>
2019-04-06 22:42:45 -0700

    saving

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,5 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import ListingPage from '../components/ListingPage.vue';
+
 Vue.use(VueRouter);
 
-export default new VueRouter();
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        // { path: '/', component: HomePage }, // Doesn't exist yet
+        {path: '/listing/:listing', component: ListingPage}
+    ]
+});
