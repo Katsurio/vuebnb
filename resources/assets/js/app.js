628d48a63b1427c41d645176f3b382b8dc6cc40f
John (Jef) Fisher <Fishe545@gmail.com>
2019-04-08 18:00:31 -0700

    fixed everything to continue where last left off

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,6 @@
 //import "core-js/fn/object/assign";
 import Vue from 'vue';
 
-import ListingPage from '../components/ListingPage.vue';
 import router from './router';
 import App from '../components/App.vue';
 
